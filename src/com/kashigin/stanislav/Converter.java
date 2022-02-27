dcd182d3f889d787e55a4b229c2dfdc3e877e2f1
Eviljellyfish <Eviljellyfish@rambler.ru>
2022-02-27 15:10:45 +0300

    Initial commit

---
+++ src/com/kashigin/stanislav/Converter.java
@@ -0,0 +1,19 @@
+package com.kashigin.stanislav;
+
+import com.digdes.school.DatesToCronConverter;
+import com.digdes.school.DatesToCronConvertException;
+
+import java.util.List;
+
+public class Converter implements DatesToCronConverter {
+
+    @Override
+    public String convert(List<String> dates) throws DatesToCronConvertException {
+        return null;
+    }
+
+    @Override
+    public String getImplementationInfo() {
+        return null;
+    }
+}
